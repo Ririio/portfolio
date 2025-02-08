3a829f41b1e119286660366ee62a26fc5dbc3a86
Ririio <xRiriio@gmail.com>
2025-02-07 22:28:15 -0500

    Initial setup of the website (#1)

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+export default function About() {
+    return (
+        <div className="flex-1 flex flex-row border ">
+            <div className="flex-1 h-full flex flex-col border justify-center items-center">
+                <h1 className="text-2xl font-bold">Mario Leonardo</h1>
+                <h1 className="text-2xl font-bold">Mario Leonardo</h1>
+            </div>
+            <div className="flex-1 flex h-full flex-col border justify-center items-center">
+                <h1 className="text-2xl font-bold">Mario Leonardo</h1>
+            </div>
+        </div>
+    )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (    
+    <div className="flex-1 flex flex-row border ">
+      <div className="flex-1 h-full flex flex-col border justify-center items-center">
+        <h1 className="text-6xl font-bold">HELLO</h1>
+      </div>
+    </div>
+  );
+}
