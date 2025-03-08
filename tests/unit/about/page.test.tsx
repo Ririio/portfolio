e9634cc39360038e9a8914917f54c3563449fa83
Mario Leonardo <xririio@gmail.com>
2025-03-07 21:45:37 -0500

    fixing linter problem

--- tests/unit/about/page.test.tsx
+++ tests/unit/about/page.test.tsx
@@ -1,11 +1,11 @@
 import '@testing-library/jest-dom'
-import {render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import Home from '@/app/about/page'
 
 describe('Page', () => {
   it('renders a heading', () => {
     render(<Home />)
- 
-    expect(screen.getByText("Hello, I am")).toBeInTheDocument();
+
+    expect(screen.getByText('Hello, I am')).toBeInTheDocument()
   })
 })
