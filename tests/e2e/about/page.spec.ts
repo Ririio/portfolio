ec45310ca4cd8b82b84d08ac1357610f2b6dfb67
Mario Leonardo <xririio@gmail.com>
2025-03-08 13:51:18 -0500

    Removing unecessary URL const

--- tests/e2e/about/page.spec.ts
+++ tests/e2e/about/page.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test'
-const URL = process.env.BASE_URL || 'http://localhost:3000/about'
 
 test('has name', async ({ page }) => {
-  await page.goto(URL)
+
+  await page.goto('/about')
 
   await expect(page.getByText('Mario Leonardo', { exact: true })).toBeVisible()
 })
